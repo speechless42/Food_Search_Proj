dfa900f1977cb0b203fedda04ecfe9c60015e951
speechless42 <94534629+speechless42@users.noreply.github.com>
2023-05-22 00:38:55 +0800

    a

--- Food_Search_Proj/Controllers/AdminController.cs
+++ Food_Search_Proj/Controllers/AdminController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Food_Search_Proj.Controllers
 {
     public class AdminController : Controller
     {
+        Models.Food_searchEntities DN = new
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
